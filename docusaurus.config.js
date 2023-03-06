94d6c2826b03bc23f24c03b9499be6d3b8d3e91e
Switch Automation <switch-automation@outlook.com>
2023-03-06 21:41:02 +0900

    feat: change icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
         title: "My Site",
         logo: {
           alt: "My Site Logo",
-          src: "img/logo.svg",
+          src: "img/logo.webp",
         },
         items: [
           {
